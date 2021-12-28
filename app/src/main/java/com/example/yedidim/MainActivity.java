58c0dc29b19981380291ce3041f33301b430afaf
yarden hovav <hovav4321@gmail.com>
2021-12-28 21:21:25 +0200

    adding a map fragment

--- app/src/main/java/com/example/yedidim/MainActivity.java
+++ app/src/main/java/com/example/yedidim/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.yedidim;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    private NavController navctrl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_edit_profile);
+        setContentView(R.layout.activity_main);
+        NavHostFragment nav_host = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.base_navHost);
+        navctrl = nav_host.getNavController();
+        NavigationUI.setupActionBarWithNavController(this, navctrl);
+
+
     }
 }
---
+++ app/src/main/java/com/example/yedidim/MapFragment.java
@@ -0,0 +1,45 @@
+package com.example.yedidim;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MapFragment extends Fragment {
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                LatLng sydney = new LatLng(31.7683, 35.2137);
+                googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Jerusalem"));
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+                LatLng ASH = new LatLng(31.8044, 34.6553);
+                googleMap.addMarker(new MarkerOptions().position(ASH).title("Marker in Ashdod"));
+                googleMap.moveCamera(CameraUpdateFactory.newLatLng(ASH));
+
+            }
+        });
+        return view;
+    }
+
+}
