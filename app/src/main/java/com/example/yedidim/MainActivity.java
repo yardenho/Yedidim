349f646f6e1a1f91fae4d1b9d5307061aa2d0e96
yarden hovav <hovav4321@gmail.com>
2021-12-29 18:38:39 +0200

    adding code in note for finding address from coordinates

--- app/src/main/java/com/example/yedidim/MainActivity.java
+++ app/src/main/java/com/example/yedidim/MainActivity.java
@@ -34,5 +34,5 @@ public class MainActivity extends AppCompatActivity {
 //        String cityName = addresses.get(0).getAddressLine(0);
 //        Log.d("TAG", "city:" + cityName);
 //
-//    }
+    }
 }
