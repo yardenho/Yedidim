78bacd8ec7c61434db6f68316840dd2548cd4622
yarden hovav <hovav4321@gmail.com>
2021-12-30 11:09:07 +0200

    adding lists to model class

--- app/src/main/java/com/example/yedidim/Model/Model.java
+++ app/src/main/java/com/example/yedidim/Model/Model.java
@@ -1,6 +1,11 @@
 package com.example.yedidim.Model;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class Model {
     static  final private Model instance = new Model();
+    private List<User> userList =new LinkedList<User>();
+    private List<Malfunction> malfunctionList =new LinkedList<Malfunction>();
 
 }
