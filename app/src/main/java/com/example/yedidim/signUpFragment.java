70f78008a4e1f054bfd6f63fb4303f6d9664ce26
chenBenTolila <chenbt266@gmail.com>
2021-12-30 16:02:57 +0200

    adding signUp fragment functionality

--- app/src/main/java/com/example/yedidim/signUpFragment.java
+++ app/src/main/java/com/example/yedidim/signUpFragment.java
@@ -2,63 +2,67 @@ package com.example.yedidim;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+
+import com.example.yedidim.Model.Model;
+import com.example.yedidim.Model.User;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link signUpFragment#newInstance} factory method to
- * create an instance of this fragment.
  */
 public class signUpFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ProgressBar pb;
 
     public signUpFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment signUpFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static signUpFragment newInstance(String param1, String param2) {
-        signUpFragment fragment = new signUpFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+        View view = inflater.inflate(R.layout.fragment_sign_up, container, false);
+
+        Button signUpBtn = getView().findViewById(R.id.signUp_btn_signup);
+        EditText userNameEt = getView().findViewById(R.id.signUp_et_username);
+        EditText passwordEt = getView().findViewById(R.id.signUp_et_password);
+        EditText vehicleBrandEt = getView().findViewById(R.id.signUp_et_vehicleBrand);
+        EditText manufactureYearEt = getView().findViewById(R.id.signUp_et_manufYear);
+        EditText fuelTypeEt = getView().findViewById(R.id.signUp_et_fuelType);
+        EditText firstNameEt = getView().findViewById(R.id.signUp_et_firstName);
+        EditText lastNameEt = getView().findViewById(R.id.signUp_et_lastName);
+        EditText phoneNumberEt = getView().findViewById(R.id.signUp_et_phoneNumber);
+
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pb.setVisibility(View.VISIBLE);
+                signUpBtn.setEnabled(false);
+                User user = new User();
+                user.setFirstName(firstNameEt.getText().toString());
+                user.setLastName(lastNameEt.getText().toString());
+                user.setUserName(userNameEt.getText().toString());
+                user.setPassword(passwordEt.getText().toString());
+                user.setVehicleBrand(vehicleBrandEt.getText().toString());
+                user.setManufactureYear(manufactureYearEt.getText().toString());
+                user.setFuelType(fuelTypeEt.getText().toString());
+                user.setPhoneNumber(phoneNumberEt.getText().toString());
+                Model.getInstance().addNewUser(user, ()->{
+                    signUpFragmentDirections.ActionSignUpFragmentToReportsListFragment action = signUpFragmentDirections.actionSignUpFragmentToReportsListFragment(user.getUserName());
+                    Navigation.findNavController(v).navigate(action);
+                });
+            }
+        });
+        return view;
     }
 }
