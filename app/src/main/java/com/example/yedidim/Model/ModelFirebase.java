d4c55b08a8a134476ed73eb664c6508464effb4d
chenBenTolila <chenbt266@gmail.com>
2022-01-03 11:06:58 +0200

    adding implementation for getUserByUserName and getReportByID in ModelFirebase

--- app/src/main/java/com/example/yedidim/Model/ModelFirebase.java
+++ app/src/main/java/com/example/yedidim/Model/ModelFirebase.java
@@ -4,9 +4,12 @@ import android.util.Log;
 
 import androidx.annotation.NonNull;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -21,7 +24,7 @@ public class ModelFirebase {
     public void addNewUser(User user, Model.addNewUserListener listener) {
         // Create a new user
         Map<String, Object> json = new HashMap<>();
-        json.put("userName", user.getUserName());
+        json.put("username", user.getUserName());
         json.put("firstName", user.getFirstName());
         json.put("lastName", user.getLastName());
         json.put("password", user.getPassword());
@@ -48,6 +51,35 @@ public class ModelFirebase {
     }
 
     public void getUserByUserName(String userName, Model.getUserByUserNameListener listener) {
+        DocumentReference docRef = db.collection("students").document(userName);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            // TODO: need to handle cases were student returns as null
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Map<String, Object> json = document.getData();
+                        User user = new User();
+                        user.setUserName((String)json.get("username"));
+                        user.setPassword((String)json.get("password"));
+                        user.setFirstName((String)json.get("firstName"));
+                        user.setLastName((String)json.get("lastName"));
+                        user.setPhoneNumber((String)json.get("phoneNumber"));
+                        user.setCarNumber((String)json.get("carNumber"));
+                        user.setVehicleBrand((String)json.get("vehicleBrand"));
+                        user.setManufactureYear((String)json.get("manufactureYear"));
+                        user.setFuelType((String)json.get("fuelType"));
+                        listener.onComplete(user);
+                    } else {
+                        listener.onComplete(null);
+                    }
+                } else {
+                    Log.d("TAG", "get failed with ", task.getException());
+                    listener.onComplete(null);
+                }
+            }
+        });
     }
 
     public void deleteUser(User user, Model.deleteUserListener listener) {
@@ -65,13 +97,13 @@ public class ModelFirebase {
         json.put("reportID", report.getReportID());
         json.put("problem", report.getProblem());
         json.put("notes", report.getNotes());
-        json.put("userName", report.getUserName());
+        json.put("username", report.getUserName());
         json.put("longitude", report.getLongitude());
         json.put("latitude", report.getLatitude());
         // TODO: need to add photo
         // json.put("image", report.getImage());
 
-        // Add a new document with a generated ID
+        // Add a new document
 
         //TODO: check that value of method works
         db.collection("reports").document(String.valueOf(report.getReportID()))
@@ -92,6 +124,33 @@ public class ModelFirebase {
     
 
     public void getReportByID(String reportID, Model.getReportByReportIDListener listener) {
+        DocumentReference docRef = db.collection("reports").document(reportID);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            // TODO: need to handle cases were student returns as null
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Map<String, Object> json = document.getData();
+                        Report report = new Report();
+                        report.setReportID((long)json.get("reportID"));   // TODO: check how to cast here
+                        report.setUserName((String)json.get("username"));
+                        report.setProblem((String)json.get("problem"));
+                        report.setNotes((String)json.get("notes"));
+                        report.setLongitude((double)json.get("longitude"));  // TODO: check how to cast here
+                        report.setLatitude((double)json.get("latitude"));  // TODO: check how to cast here
+                        listener.onComplete(report);
+
+                    } else {
+                        listener.onComplete(null);
+                    }
+                } else {
+                    Log.d("TAG", "get failed with ", task.getException());
+                    listener.onComplete(null);
+                }
+            }
+        });
     }
 
     public void deleteReport(Report report, Model.deleteReportListener listener) {
