702c1f43ff80c4c214c9a3497e4f9ccd96092e39
yarden hovav <hovav4321@gmail.com>
2022-02-17 18:48:39 +0200

    adding new fragment, the first fragment

---
+++ app/src/main/java/com/example/yedidim/firstFragment.java
@@ -0,0 +1,26 @@
+package com.example.yedidim;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class firstFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+
+        return view;
+    }
+}
