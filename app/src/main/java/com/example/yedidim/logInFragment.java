b02511618c8718f6a2cd1fb16854489dc0ec2fe1
chenBenTolila <chenbt266@gmail.com>
2022-02-17 18:42:02 +0200

    fixing problem in login fragment

--- app/src/main/java/com/example/yedidim/logInFragment.java
+++ app/src/main/java/com/example/yedidim/logInFragment.java
@@ -24,10 +24,10 @@ public class logInFragment extends Fragment {
     EditText usernameEt;
     EditText passwordEt;
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState)
+    {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_log_in, container, false);
 
@@ -49,26 +49,21 @@ public class logInFragment extends Fragment {
             public void onClick(View v) {
                 pb.setVisibility(View.VISIBLE);
                 logInBtn.setEnabled(false);
+                cancelBtn.setEnabled(false);
                 if (checkDetails()) {
-                    logInBtn.setEnabled(false);
-                    cancelBtn.setEnabled(false);
                     Model.getInstance().loginUser(usernameEt.getText().toString().trim(),
                             passwordEt.getText().toString().trim(), new Model.loginUserListener() {
                                 @Override
                                 public void onComplete(boolean success) {
-                                    if(success)
-                                    {
-                                    pb.setVisibility(View.VISIBLE);
-                                    if(success)
-                                    {
+                                    if(success) {
                                         logInFragmentDirections.ActionLogInFragmentToReportsListFragment action = logInFragmentDirections.actionLogInFragmentToReportsListFragment(usernameEt.getText().toString());
                                         Navigation.findNavController(v).navigate(action);
                                     }
-                                    else
-                                    {
+                                    else {
                                         Toast.makeText(getActivity(), "failed to login, please check your credentials", Toast.LENGTH_LONG).show();
                                         pb.setVisibility(View.GONE);
                                         logInBtn.setEnabled(true);
+                                        cancelBtn.setEnabled(true);
                                     }
                                 }
                             });
@@ -77,6 +72,7 @@ public class logInFragment extends Fragment {
                 {
                     pb.setVisibility(View.GONE);
                     logInBtn.setEnabled(true);
+                    cancelBtn.setEnabled(true);
                 }
             }
         });
@@ -109,4 +105,93 @@ public class logInFragment extends Fragment {
         }
         return true;
     }
+
+
+//    @Override
+//    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+//                             Bundle savedInstanceState) {
+//        // Inflate the layout for this fragment
+//        View view = inflater.inflate(R.layout.fragment_log_in, container, false);
+//
+//        usernameEt = view.findViewById(R.id.logIn_et_username);
+//        passwordEt = view.findViewById(R.id.logIn_et_password);
+//        Button logInBtn = view.findViewById(R.id.logIn_btn_logIn);
+//        Button cancelBtn = view.findViewById(R.id.logIn_btn_cancel);
+//        ProgressBar pb = view.findViewById(R.id.logIn_progressBar);
+//        pb.setVisibility(View.GONE);
+//        cancelBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Navigation.findNavController(v).navigateUp();
+//            }
+//        });
+//
+//        logInBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                pb.setVisibility(View.VISIBLE);
+//                logInBtn.setEnabled(false);
+//                if (checkDetails()) {
+//                    logInBtn.setEnabled(false);
+//                    cancelBtn.setEnabled(false);
+//                    Model.getInstance().loginUser(usernameEt.getText().toString().trim(),
+//                            passwordEt.getText().toString().trim(), new Model.loginUserListener() {
+//                                @Override
+//                                public void onComplete(boolean success) {
+//                                    if(success)
+//                                    {
+//                                        pb.setVisibility(View.VISIBLE);
+//                                        if(success)
+//                                        {
+//                                            logInFragmentDirections.ActionLogInFragmentToReportsListFragment action = logInFragmentDirections.actionLogInFragmentToReportsListFragment(usernameEt.getText().toString());
+//                                            Navigation.findNavController(v).navigate(action);
+//                                        }
+//                                        else
+//                                        {
+//                                            Toast.makeText(getActivity(), "failed to login, please check your credentials", Toast.LENGTH_LONG).show();
+//                                            pb.setVisibility(View.GONE);
+//                                            logInBtn.setEnabled(true);
+//                                    }
+//                                    }
+//                                });
+//                    }
+//                }
+//                else
+//                {
+//                    pb.setVisibility(View.GONE);
+//                    logInBtn.setEnabled(true);
+//                }
+//        });
+//        return view;
+//    }
+//
+//
+//    private boolean checkDetails() {
+//        String email = usernameEt.getText().toString().trim();
+//        String password = passwordEt.getText().toString().trim();
+//        if (email.isEmpty()) {
+//            usernameEt.setError("email is required");
+//            usernameEt.requestFocus();
+//            return false;
+//        }
+//        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+//            usernameEt.setError("Please provide valid email");
+//            usernameEt.requestFocus();
+//            return false;
+//        }
+//
+//        if (password.isEmpty()) {
+//            passwordEt.setError("password is required");
+//            passwordEt.requestFocus();
+//            return false;
+//        }
+//        if (password.length() < 6) {
+//            passwordEt.setError("password should be at least 6 characters");
+//            passwordEt.requestFocus();
+//            return false;
+//        }
+//        return true;
+//    }
+//    }
+//}
 }
